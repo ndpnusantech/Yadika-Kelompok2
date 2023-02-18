70b6f10b72270a965a1772e975d2cbfaec00cb2b
yantiandriani <yantiindriani691@gmail.com>
2023-02-18 11:50:00 +0700

    Yanti - Halaman Login

---
+++ project-film-react/src/components/login/login.js
@@ -0,0 +1,36 @@
+import "./login.css";
+
+const Login = () => {
+  const logo = "./assets/logo/logoo 2.png";
+
+  return (
+    <div className="box">
+      <div className="logo">
+        <img
+          src={logo}
+          alt=""
+          width="400px"
+          heigt="350px"
+          style={{ marginBottom: "50px", marginTop: "60px" }}
+        />
+      </div>
+      <div className="form">
+        <form action="">
+          <input type="text" placeholder="Username" required />
+          <br />
+          <br />
+          <br />
+          <input type="password" placeholder="Password" required />
+        </form>
+        <div className="button">
+          <button type="submit">Login</button>
+        </div>
+        <p>
+          Dont have an account? <a href="/register">Register</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ project-film-react/src/components/pages/loginPage.js
@@ -0,0 +1,11 @@
+import Login from "../login/login";
+import "./style.css";
+const LoginPage = () => {
+  return (
+    <div className="loginBox">
+      <Login />
+    </div>
+  );
+};
+
+export default LoginPage;
