4cee9b5db06134941a04430c1ff63a60a99488ea
yantiandriani <yantiindriani691@gmail.com>
2023-03-01 15:33:02 +0700

    Nazya Yanti - Menambahkan halaman pembayaran

--- src/components/Pembayaran/pembayaran.js
+++ src/components/Pembayaran/pembayaran.js
@@ -23,14 +23,44 @@ const Pembayaran = () => {
         />
       </div>
       <div className="boxPembayaran"></div>
+      <h1>PAYMENT</h1>
       <div className="titlePay">
-        <h1>PAYMENT</h1>
         <div className="mx-auto d-flex justify-content-center">
-          <Button variant="light" title="qris">
+          <Button className="btnQris" variant="light" title="qris">
             <img
               src="./assets/logo/logoqris.png"
-              className="btn-img"
+              className="btn-img-qris"
               width={100}
+              alt="img"
+            />
+          </Button>
+        </div>
+        <div className="mx-auto d-flex justify-content-center">
+          <Button className="btnGopay" variant="light" title="gopay">
+            <img
+              src="./assets/logo/logoGopay.png"
+              className="btn-img-gopay"
+              width={50}
+              alt="img"
+            />
+          </Button>
+        </div>
+        <div className="mx-auto d-flex justify-content-center">
+          <Button className="btnSpay" variant="light" title="spay">
+            <img
+              src="./assets/logo/spay.png"
+              className="btn-img-spay"
+              width={80}
+              alt="img"
+            />
+          </Button>
+        </div>
+        <div className="mx-auto d-flex justify-content-center">
+          <Button className="btnDana" variant="light" title="dana">
+            <img
+              src="./assets/logo/Dana.png"
+              className="btn-img-dana"
+              width={80}
               alt="img"
             />
           </Button>
