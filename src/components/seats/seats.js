daefc7696084cf9270781cfcb5c8e02bd7fed93f
sabilah Mudrikah <sabilahmudrikah@gmail.com>
2023-03-15 14:18:08 +0700

    sabilah menghapus button price di halaman tiket

--- src/components/seats/seats.js
+++ src/components/seats/seats.js
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 import ChairBtn from "../chair/ChairBtn";
-import "./seats.css"
-import { dataChair1 } from "../dataChair/dataChair1"
-import { dataChair2 } from "../dataChair/dataChair2"
-import Button from "react-bootstrap/Button"
-
+import "./seats.css";
+import { dataChair1 } from "../dataChair/dataChair1";
+import { dataChair2 } from "../dataChair/dataChair2";
+import Button from "react-bootstrap/Button";
 
 const Seat = () => {
-    const [dfbgbt , setclbt ] = useState("white")
-    const [valueBtn, setValueBtn] = useState(false);
+  const [dfbgbt, setclbt] = useState("white");
+  const [valueBtn, setValueBtn] = useState(false);
 
   const changeValue = () => {
     if (dfbgbt === "white") {
@@ -20,7 +19,6 @@ const Seat = () => {
 
   return (
     <div className="full-seats">
-       
       <center>
         <div className="wrap-logo">
           <img src="../assets/logo/logoo 2.png" alt="" />
@@ -31,12 +29,12 @@ const Seat = () => {
       </center>
       <div className="wrap-seats">
         <div className="seats-k-1">
-        {dataChair1.map((data) => (
-          <ChairBtn
-            noSeat={data.chair}
-            onClick={() => changeValue()}
-            value={valueBtn}
-          />
+          {dataChair1.map((data) => (
+            <ChairBtn
+              noSeat={data.chair}
+              onClick={() => changeValue()}
+              value={valueBtn}
+            />
           ))}
         </div>
         <div className="seats-k-2">
@@ -44,23 +42,17 @@ const Seat = () => {
             <ChairBtn
               noSeat={data.chair}
               onClick={() => changeValue()}
-            value={valueBtn}
+              value={valueBtn}
             />
-            ))}
+          ))}
         </div>
       </div>
 
       <div className="btn-seats">
-            <Button href="/pembayaranPage" variant="light">CONFIRM</Button>
-
-      {valueBtn === true && (
-       <div className="btn-seats" >
-            <Button href="/pembayaranPage" variant="light">PRICE Rp 40.000 CONFRIM</Button>
-
+        <Button href="/pembayaranPage" variant="light">
+          CONFIRM
+        </Button>
       </div>
-      )}  
- 
-    </div>
     </div>
   );
 };
