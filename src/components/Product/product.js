2dc31f37638bd667134ae0fdb6d8045d3f5b1753
Fachriiii Firdaus <fachri4606@gmail.com>
2023-03-30 07:41:10 +0700

    pull sabilah

--- src/components/Product/product.js
+++ src/components/Product/product.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Button,  Table} from "react-bootstrap";
+import { Button,  Table , Form} from "react-bootstrap";
 import "./product.css"
 const Product = () => {
   const [data, setData] = useState(() => {
@@ -10,6 +10,9 @@ const Product = () => {
     ];
   });
  
+  const [editData, setEditData] = useState(null);
+
+
 
   useEffect(() => {
     localStorage.setItem("movieData", JSON.stringify(data));
@@ -19,7 +22,30 @@ const Product = () => {
     const newData = data.filter((item) => item.id !== id);
     setData(newData);
   };
+  const handleEdit = (id) => {
+    const newData = data.find((item) => item.id === id);
+    setEditData(newData);
+  };
 
+  const handleSave = (event) => {
+    event.preventDefault();
+    const newData = data.map((item) => {
+      if (item.id === editData.id) {
+        return editData;
+      }
+      return item;
+    });
+    setData(newData);
+    setEditData(null);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setEditData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
  const logo ="/assets/logo/logoo 2.png"
 
@@ -62,7 +88,7 @@ const Product = () => {
               <td className="td-admin">{item.lokasi}</td>
               <td className="td-admin">{item.status}</td>
               <td className="td-admin">
-
+              <Button className="btn-edit-product-admin" variant="light" onClick={() => handleEdit(item.id)}>Edit</Button>
                 <Button className="btn-delete-product-admin" variant="light" onClick={() => handleDelete(item.id)}>Delete</Button>
                 
 
@@ -75,7 +101,83 @@ const Product = () => {
         </tbody>
       </Table>
 
-
+      {editData && (
+        <Form onSubmit={handleSave} className="edit-form-admin">
+          <label>
+            Title:
+            <input
+              type="text"
+              name="title"
+              value={editData.title}
+              onChange={handleChange}
+              className="input-product-admin"
+            />
+          </label>
+          <label>
+            Director:
+            <input
+              type="text"
+              name="director"
+              value={editData.director}
+              onChange={handleChange}
+              className="input-product-admin"
+            />
+          </label>
+          <label>
+            Genre:
+            <input
+              type="text"
+              name="genre"
+              value={editData.genre}
+              onChange={handleChange}
+              className="input-product-admin"
+            />
+          </label>
+          <label>
+            Stok:
+            <input
+              type="number"
+              name="stok"
+              value={editData.stok}
+              onChange={handleChange}
+              className="input-product-admin"
+            />
+          </label>
+          <label>
+            Penayangan:
+            <input
+              type="text"
+              name="tayang"
+              value={editData.tayang}
+              onChange={handleChange}
+              className="input-product-admin"
+            />
+          </label>
+          <label>
+            Lokasi:
+            <input
+              type="text"
+              name="lokasi"
+              value={editData.lokasi}
+              onChange={handleChange}
+              className="input-product-admin"
+            />
+          </label>
+          <label>
+            Status:
+            <input
+              type="text"
+              name="status"
+              value={editData.status}
+              onChange={handleChange}
+              className="input-product-admin"
+            />
+          </label>
+          <Button className="btn-save-product-admin" variant="light" type="submit">
+            Save
+          </Button>
+        </Form>
+      )}
     </div>
     </>
   )
--- src/components/layouts/navbar/navbar.js
+++ src/components/layouts/navbar/navbar.js
@@ -10,8 +10,8 @@ const NavigationBar = () => {
       <Navbar bg="dark" variant="dark" className='p-3 navigation' >
         <Container>
           <Nav className="me-auto">
-            <Nav.Link href="/nowPlaying">Now Playing</Nav.Link>
-            <Nav.Link href="/upComing" >Up Coming</Nav.Link>
+            <Nav.Link href="/nowPlaying" className="txt-nav-link">Now Playing</Nav.Link>
+            <Nav.Link href="/upComing" className="txt-nav-link">Up Coming</Nav.Link>
           </Nav>
 
           <Form className="d-flex" >
