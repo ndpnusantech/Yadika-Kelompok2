56cf3fb1012f4694c0fe1e001cc740fa9c88d25f
Fachriiii Firdaus <fachri4606@gmail.com>
2023-06-11 10:57:56 +0700

    test be

--- backend/models/HargaProdukModel.js
+++ backend/models/HargaProdukModel.js
@@ -3,6 +3,7 @@ import database from "../config/database.js"
 
 const { DataTypes } = Sequelize;
 
+
 const hargaProduk = database.define('harga_produk',{
     id_hargafilm:DataTypes.INTEGER,
     weekend_film:DataTypes.STRING,
